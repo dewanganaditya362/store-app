eb5f911d989d5e0c6c634fbc7382adfb3a41b058
Aditya <dewanganaditya362@gmail.com>
2020-06-25 15:54:06 +0530

    links to font awesome and google fonts and bootsrap added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div>
-      
-    </div>
+
+
+class App extends Component{
+  render(){
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            hello mate!
+          </div>
+          <div className="col-6">
+            hello there!
+            <span>
+              <i className="fas fa-home"/>
+            </span>
+          </div>
+        </div>
+      </div>
+
   );
+  }
+ 
 }
 
 export default App;
