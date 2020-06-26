148cf1ba41e1d7ea5456b6dbe596749c25219717
Aditya <dewanganaditya362@gmail.com>
2020-06-26 15:23:05 +0530

    added react router dom component to link various pages

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import React, {Component} from 'react';
-
+import React, {Component} from 'react'
+import logo from './logo.svg'
+import "./App.css"
+import {Switch, Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import ProductList from './components/ProductList'
+import Details from './components/Details'
+import Cart from './components/Cart'
+import Default from './components/Default'
 
 
 class App extends Component{
   render(){
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-6">
-            hello mate!
-          </div>
-          <div className="col-6">
-            hello there!
-            <span>
-              <i className="fas fa-home"/>
-            </span>
-          </div>
-        </div>
-      </div>
-
-  );
+    <React.Fragment>
+      <Navbar/>
+      <Switch>
+        <Route exact path="/" component={ProductList}/>
+        <Route exact path="/details" component={Details}/>
+        <Route exact path="/cart" component={Cart}/>
+        <Route component={Default}/>
+      </Switch>
+    </React.Fragment>
+  )
   }
  
 }
 
-export default App;
+export default App
