e56abf567d92a0e8186c9a381f0637e551055e3b
Aditya <dewanganaditya362@gmail.com>
2020-06-29 16:52:09 +0530

    added images of products and cart button

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+import {Link} from 'react-router-dom'
+import {ProductConsumer} from './Context'
 
 export default class Product extends Component {
     render() {
+
+        const {id,title, img , price , inCart} = this.props.product;
         return (
-            <div>
-                <h3>hello from products</h3>
-            </div>
+            <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+                <div className="card">
+                    <div className="img-container p-5">
+                        <Link to="/details">
+                            <img src={img} alt="product" className="card-img-top"/>
+                        </Link>
+                    </div>
+                    <button className="cart-btn" disabled={inCart?true:false}>
+                        {inCart ? (<p className="text-captalize mb-0" disabled>
+                            InCart
+                        </p>): (<i className="fas fa-cart-plus"/>)}
+                    </button>
+                </div>
+            </ProductWrapper>
         )
     }
 }
+
+const ProductWrapper = styled.div`
+`;
